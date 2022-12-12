e36578b44a62e878b1a346067100c2098b6cd741
Malachyyy <103989250+Malachyyy@users.noreply.github.com>
2022-12-12 15:29:05 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,41 +2,46 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Display the login form
-		fmt.Fprintf(w, `
-        <html>
-             <body>
-                <h1>Login</h1>
-                <form action="/login" method="post">
-                    <label for="username">Username:</label><br>
-                    <input type="text" id="username" name="username"><br>
-                    <label for="password">Password:</label><br>
-                    <input type="password" id="password" name="password"><br><br>
-                    <input type="submit" value="Submit">
-                </form> 
-            </body>
-        </html>
-     `)
-	})
-
-	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-		// Get the username and password from the request
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-
-		if username == "admin" && password == "password" {
-			// If authentication is successful, return a success message
-			fmt.Fprintf(w, "Login successful!")
-		} else {
-			// If authentication fails, return an error message
-			http.Error(w, "Invalid username or password", http.StatusUnauthorized)
-		}
-	})
+	http.HandleFunc("/", BeginScherm)
+	http.HandleFunc("/Account", Account)
+	http.HandleFunc("/Register", RegisterHandler)
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func BeginScherm(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFiles("loginform.html")
+	if err != nil {
+		panic(err)
+	}
+	tpl.ExecuteTemplate(w, "loginform.html", nil)
+
+	http.Redirect(w, r, "/Register", )
+}
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	tpl, err := template.ParseFiles("Regristratieform.html")
+	if err != nil {
+		panic(err)
+	}
+	tpl.ExecuteTemplate(w, "Regristratieform.html", nil)
+}
+
+func Account(w http.ResponseWriter, r *http.Request) {
+	// Get the username and password from the request
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	if username == "admin" && password == "password" {
+		// If authentication is successful, return a success message
+		fmt.Fprintf(w, "Login successful!")
+	} else {
+		// If authentication fails, return an error message
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+	}
 }
